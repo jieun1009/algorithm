c9f59a3140370015a65ffde0f0f457550173482b
zeun <real_jieun99@daum.net>
2023-05-08 23:31:56 +0900

    DP 문제 풀이 두번째

--- BOJ/[DP]RGB거리.py
+++ BOJ/[DP]RGB거리.py
@@ -1,16 +1,15 @@
-n =  int(input())
+n = int(input())
 
-arr =[]
-for _ in range(n):
-    arr.append(list(map(int, input().split())))
-    # 0 빨 1 초 2 파
+arr=[]
+for i in range(n):
+    arr.append(list(map(int, input().split()))) # 0 빨 1 초 2 파
 
-for i in range(1, n):
-    # 빨간집을 선택한 경우 : 이전 선택이 초/파
-    arr[i][0] = min(arr[i-1][1], arr[i-1][2]) + arr[i][0]
-    # 초록집 : 이전 선택이 빨/파
+for i in range(1,len(arr)):
+    # 빨강 선택
+    arr[i][0] = min(arr[i-1][1], arr[i-1][2])+arr[i][0]
+    # 초록 선택
     arr[i][1] = min(arr[i-1][0], arr[i-1][2])+arr[i][1]
-    # 파란집
+    # 파랑 선택
     arr[i][2] = min(arr[i-1][0], arr[i-1][1])+arr[i][2]
     
 print(min(arr[n-1]))
