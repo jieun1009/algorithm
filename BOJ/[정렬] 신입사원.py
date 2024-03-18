efc386460224a96b0f0be009db07a4a00a04ccae
zeunxx <real_jieun99@daum.net>
2024-03-18 14:16:18 +0900

    백준 그리디 문제풀이

--- BOJ/[정렬] 신입사원.py
+++ BOJ/[정렬] 신입사원.py
@@ -1,23 +1,20 @@
-t = int(input())
+import sys
+
+t = int(sys.stdin.readline())
 
 for _ in range(t):
-    n = int(input())
+    n = int(sys.stdin.readline())
     arr = []
     for i in range(n):
-        s, m = map(int, input().split())
+        s, m = map(int, sys.stdin.readline().split())
         arr.append((s,m))
 
     arr.sort(key = lambda x: x[0])
+    cnt =1
+    rank = arr[0][i] # 서류 1등이 기준
+    for i in range(n):
+        if arr[i][1] < rank : # 면접 등수가 이전 사람보다 높으면(arr은 서류 기준으로 정렬)
+            cnt+=1
+            rank = arr[i][1]
 
-    answer = 1
-    answerarr = []
-    maxvalue = 0
-    for i in range(n-1):
-        maxvalue = max(maxvalue, arr[i][1])
-        if maxvalue > arr[i+1][1]:
-            answer +=1
-        else:
-            answerarr.append(answer)
-            answer = 1
-    answerarr.append(answer)
-    print(max(answerarr))
+    print(cnt)
